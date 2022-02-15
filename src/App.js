92d77c10adb98ad9c308ecd598c513fda6e1d464
taoufik ahjam <taoufikahjam@gmail.com>
2022-02-15 14:33:52 +0100

    first3 commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and firebase 2
+          Learn React and firebase 3
         </a>
       </header>
     </div>
